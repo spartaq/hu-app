f793d4bc6c0b0e73a46e27ec539318e1cb73cb7d
SpartaQ <theshiek@gmail.com>
2025-11-25 22:11:40 +0100

    home order

--- src/Mainpages/Home.js
+++ src/Mainpages/Home.js
@@ -16,11 +16,11 @@ const Home = ({ setFilters = () => {} }) => {
 
   const actions = [
   { label: "Vocabulary", icon: <FaBook />, link: "/hungarian/vocabularyHub" },
+  { label: "Readings", icon: <FaBookReader />, link: "/hungarian/readingHub" },
+  { label: "Quizzes", icon: <FaClipboardList />, link: "/hungarian/quizHub" },
   { label: "Grammar", icon: <FaListUl />, link: "/hungarian/grammar-list" },
   { label: "Listening", icon: <FaHeadphones />, link: "/hungarian/listening-list" },
   { label: "Dialogues", icon: <FaComments />, link: "/hungarian/dialogue-list" },
-  { label: "Readings", icon: <FaBookReader />, link: "/hungarian/readingHub" },
-  { label: "Quizzes", icon: <FaClipboardList />, link: "/hungarian/quizHub" },
   { label: "Games", icon: <FaPuzzlePiece />, link: "/hungarian/games" }
 ];
 
